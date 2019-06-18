6d4eae7d4405732beae26a899d4e083fb92eb680
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-06-18 11:25:43 +0530

    Create lcm.py

---
+++ lcm.py
@@ -0,0 +1,16 @@
+def lcm(x, y):
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+num1,num2 = [int(num1) for num1 in input().split()]
+print( lcm(num1, num2))
