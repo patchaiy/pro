3f55395f65fdca1d59f97356089e4f28b3782bc3
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-06-18 19:58:41 +0530

    Create facts.py

---
+++ facts.py
@@ -0,0 +1,14 @@
+def factorial(num):
+    
+    if num == 1:
+        return num
+    else:
+        return num * factorial(num - 1)
+
+
+num = int(input())
+
+if num == 0:
+    print("1")
+else:
+    print(factorial(num))
