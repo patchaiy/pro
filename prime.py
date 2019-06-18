a417218b7147ff3dd76cda64cfbe8dc5684314e0
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-06-18 18:18:32 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,18 @@
+num = int(input())
+  
+ 
+if num > 1: 
+      
+    
+   for i in range(2, num//2): 
+         
+     
+       
+       if (num % i) == 0: 
+           print( "no") 
+           break
+   else: 
+       print( "yes") 
+  
+else: 
+   print( "yes") 
