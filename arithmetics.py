3440b9dc10ba77b8c738815b23ecdb5184968742
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-06-28 19:16:59 +0530

    Update arithmetics.py

--- arithmetics.py
+++ arithmetics.py
@@ -1,6 +1,6 @@
-def  sumOfAP(N,  A,  D): 
-  sum = (N/ 2) * (2 * A + (N - 1) * D) 
-  return sum  
+def  sumap(N,  A,  D): 
+  sum1= (N/ 2) * (2 * A + (N - 1) * D) 
+  return sum1  
 N, A, D = [int(N) for N in input().split()] 
-print(sumOfAP(N, A, D)) 
+print(sumap(N, A, D)) 
   
