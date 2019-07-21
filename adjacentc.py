217e43fa95723b566672efc657d8a18cae39b161
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-07-21 18:05:46 +0530

    Update adjacentc.py

--- adjacentc.py
+++ adjacentc.py
@@ -1,28 +1,28 @@
-ppi,qq3=input().split()
-ppi=int(ppi)
-qqi=int(qq3)
+gp,st=input().split()
+gp=int(gp)
+st=int(st)
 s3=''
-u3=2
-if(ppi+qq3<=3):
-    for i in range(0,ppi+qq3):
+u=2
+if(gp+st<=3):
+    for i in range(0,gp+st):
         if(i%2!=0):
             s3=s3+'0'
         else:
             s3=s3+'1'
 else:    
-    for i in range(0,ppi+qq3):
+    for i in range(0,gp+st):
         if(i==u3):
             s3=s3+'0'
-            if(u3==qq3):
+            if(u3==st):
                 u3=u3+2
             else:
                 u3=u3+3
         else:
             s3=s3+'1'
-x=len(s3)-1
-if(int(s3[x])==0):
+x1=len(s3)-1
+if(int(s3[x1])==0):
     print('-1') 
-elif ppi==1 and qq3==2: 
+elif gp==1 and st==2: 
      print("011")
 else:
     print(s3)
